f39277dec52ec0121f5fb81f77fb4db6700be274
Omkar Patole <omkarpatole1799@gmail.com>
2022-12-02 16:21:38 +0530

    added bootstrap and imported components

--- src/Commponent/FooterComponent.js
+++ src/Commponent/FooterComponent.js
@@ -1,1 +1,12 @@
 //this is footer component
+import React from 'react';
+
+function FooterComponent(){
+    return(
+        <>
+        <h1>Footer Component</h1>
+        </>
+    )
+}
+
+export default FooterComponent
--- src/Commponent/ProjectComponent.js
+++ src/Commponent/ProjectComponent.js
@@ -1,1 +1,12 @@
 //this is project component
+import React from 'react';
+
+function ProjectComponent(){
+    return(
+        <>
+        <h1>Project Component</h1>
+        </>
+    )
+}
+
+export default ProjectComponent
