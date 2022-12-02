d46be71b1703f9be8465ba61bf19287fdab41110
Omkar Patole <omkarpatole1799@gmail.com>
2022-12-02 15:49:55 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './Commponent/Nav';
+import HomeComponent from './Commponent/HomeComponent';
+import SkillComponent from './Commponent/SkillComponent';
+import ProjectComponent from './Commponent/ProjectComponent';
+import ContactComponent from './Commponent/ContactComponent';
+import FooterComponent from './Commponent/FooterComponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav/>
+    <HomeComponent/>
+    <SkillComponent/>
+    <ProjectComponent/>
+    <ContactComponent/>
+    <FooterComponent/>
+
+    </>
   );
 }
 
